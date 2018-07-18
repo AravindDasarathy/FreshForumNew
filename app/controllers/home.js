17fd5ec366328e7906acf9a8a28a86e1e47eb713
Aravind Dasarathy <aravindd@FDLMC1847.local>
2018-07-18 22:14:05 +0530

    First commit

---
+++ app/controllers/home.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+import $ from 'jquery';
+export default Controller.extend({
+	question_value : '',
+	submit_response_message : '',
+	display_question : '',
+	question_area_id : '',
+  actions : {
+		save_and_post_question() {
+			const question = this.get('question_value');
+			// const new_question = this.store.createRecord('question-model', {question : question});
+			this.store.createRecord('question-model', {question : question});
+			// new_question.save();
+			this.set('submit_response_message','Thank You! Your question has been successfully posted!');
+			this.set('display_question',question);
+			this.set('question_value','');
+			var div_q = $('#question_area_id');
+			div_q.append(`<p class = 'question_text'>${question}</p>`);
+			$('#response_message').fadeIn('fast').delay(500).fadeOut('slow');
+		}
+	}
+});
--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('home');
+  this.route('questions');
+  this.route('anwers');
+  this.route('answers');
 });
 
 export default Router;
